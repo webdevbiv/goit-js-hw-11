613ecb8ad8d85a7f636f5d6415b8a6b0a333398b
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-12-11 23:09:10 +0200

    basic css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './sass/main.scss';
+
+
+// const axios = require('axios');
+
+// axios.get('/user?ID=12345')
+//     .then(function (response) {
+//         // handle success
+//         console.log(response);
+//     })
+//     .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//     })
